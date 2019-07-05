f5f5ebd50b039186d1f4e60b2bb2b97a2c8e4cd6
JunMorimoto <morijun0806@gmail.com>
2019-07-05 15:17:07 +0000

    Before Error Message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,18 +9,40 @@ class BooksController < ApplicationController
 
   def create
   	book = Book.new(book_params)
-  	book.save
-  	redirect_to book_path(book)
+  	if book.save
+  		flash[:notice] = "Book was successfully created."
+  		redirect_to book_path(book)
+  	else
+  		render :index
+  	end
   end
 
   def show
   	@book = Book.find(params[:id])
   end
 
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
   def update
+  	book = Book.find(params[:id])
+  	if book.update(book_params)
+  		flash[:notice] = "Book was successfully created."
+  		redirect_to book_path(book)
+  	else
+  		render :edit
+  	end
   end
 
   def destroy
+  	book = Book.find(params[:id])
+  	if book.destroy
+  		flash[:notice] = "Book was successfully deleted."
+  		redirect_to books_path
+  	else
+  		render :index
+  	end
   end
 
   private
